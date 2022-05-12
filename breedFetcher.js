5d43b9141b37c416b1cad31bf60dbfa9c82f2f90
Maddy Tan <maddytan7@gmail.com>
2022-05-12 16:55:27 +0000

    created index file and wrapped breedFetcher in a new function to be called in index

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,23 +1,23 @@
 const request = require('request');
-const argArray = process.argv.slice(2);
-const breed = argArray[0];
 
-let newUrl = 'https://api.thecatapi.com/v1/breeds/search?q=' + breed.toString();
+const fetchBreedDescription = (breedName, callback) => {
+  let newUrl = 'https://api.thecatapi.com/v1/breeds/search?q=' + breedName.toString();
 
-request(newUrl, (error, response, body) => {
-  if (error) {
-    console.log(error);
-    return error;
-  }
-
-  const data = JSON.parse(body);
-
-
-  if (!data[0]) {
-    console.log('breed not found');
-  } else {
-    for (let i of data) {
-      console.log(i.description);
+  request(newUrl, (error, response, body) => {
+    if (error) {
+      callback(error);
     }
-  }
-});
+
+    const data = JSON.parse(body);
+
+    if (!data[0]) {
+      callback(error, "breed not found");
+    } else {
+      for (let i of data) {
+        callback(null, i.description);
+      }
+    }
+  });
+};
+
+module.exports = { fetchBreedDescription };
