062a29133b2ed95490c7764c0c2b8970a1c93c1c
Maddy Tan <maddytan7@gmail.com>
2022-05-12 03:02:33 +0000

    added request error message

--- breedFetcher.js
+++ breedFetcher.js
@@ -6,17 +6,18 @@ let newUrl = 'https://api.thecatapi.com/v1/breeds/search?q=' + breed.toString();
 
 request(newUrl, (error, response, body) => {
   if (error) {
-    console.log("error: ", error);
-  };
+    console.log(error);
+    return error;
+  }
 
   const data = JSON.parse(body);
-  if(!data[0]) {
+  if (!data[0]) {
     console.log('breed not found');
   } else {
     for (let i of data) {
       console.log(i);
-    };
-  }; 
+    }
+  }
 
 
 
